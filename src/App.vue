ab9e38d292a7c8e57e2a7fc9865b319c56c8899c
Quentin <quentin.francois75@gmail.com>
2022-11-14 02:39:54 +0100

    :tada: Base template

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+	<Header title="Starter KIT"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+defineProps({
+	title: String
+})
+
+</script>
+
+<template>
+	<header>
+		<div class="header-part header-left">
+			<h1>{{ title }}</h1>
+		</div>
+		<div class="header-part header-right">
+
+		</div>
+	</header>
+</template>
+
+<style lang="scss" scoped>
+
+@use "src/scss/variables" as vr;
+
+header {
+	min-height: 100vh;
+	display: flex;
+	align-items: stretch;
+
+	.header-part{
+		flex: 1;
+	}
+
+	.header-left{
+		display: flex;
+		flex-direction: column;
+		gap: vr.$space-1;
+	}
+}
+</style>
